803e107ab88ad2a87831e349a159e841928dc9f2
Seungho Kim <niceb5y@gmail.com>
2019-11-06 07:56:01 +0900

    Update RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
+                  description: edge.node.frontmatter.description || edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.frontmatter.url,
                   guid: site.siteMetadata.siteUrl + edge.node.frontmatter.url,
@@ -96,6 +96,7 @@ module.exports = {
                         title
                         date
                         url
+                        description
                       }
                     }
                   }
