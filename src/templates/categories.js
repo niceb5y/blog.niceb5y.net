3888fd444ebfe2df319bc5a056a7d054cfd93f58
Seungho Kim <niceb5y@gmail.com>
2019-11-13 09:17:40 +0900

    Bump version Update category list order

--- src/templates/categories.js
+++ src/templates/categories.js
@@ -61,6 +61,7 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { categories: { eq: $categories } } }
     ) {
       edges {
