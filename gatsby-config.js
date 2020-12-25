e864b8b5496e20679d6c09fcad0f50c65b4e840e
Seungho Kim <niceb5y@gmail.com>
2020-12-25 22:39:27 +0900

    Update copyright

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,24 +6,24 @@ module.exports = {
     siteUrl: `https://blog.niceb5y.net/`,
     image: `/images/blog.png`,
     social: {
-      twitter: `niceb5y`
+      twitter: `niceb5y`,
     },
-    copyright: `© 2014-2020 niceb5y`
+    copyright: `© 2014-2021 niceb5y`,
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`
-      }
+        name: `blog`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`
-      }
+        name: `assets`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -33,28 +33,28 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1100,
-              quality: 80
-            }
+              quality: 80,
+            },
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
-            }
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`
-        ]
-      }
+          `gatsby-remark-smartypants`,
+        ],
+      },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-80539013-1`
-      }
+        trackingId: `UA-80539013-1`,
+      },
     },
     {
       resolve: `gatsby-plugin-feed`,
@@ -74,13 +74,13 @@ module.exports = {
         feeds: [
           {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
+              return allMarkdownRemark.edges.map((edge) => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.frontmatter.description,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.frontmatter.url,
                   guid: site.siteMetadata.siteUrl + edge.node.frontmatter.url,
-                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
                 })
               })
             },
@@ -106,10 +106,10 @@ module.exports = {
             `,
             output: '/index.xml',
             title: 'niceb5y blog',
-            match: '^/blog/'
-          }
-        ]
-      }
+            match: '^/blog/',
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -120,14 +120,14 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#0075C1`,
         display: `minimal-ui`,
-        icon: `content/assets/icon.png`
-      }
+        icon: `content/assets/icon.png`,
+      },
     },
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://blog.niceb5y.net`
-      }
+        siteUrl: `https://blog.niceb5y.net`,
+      },
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
@@ -135,6 +135,6 @@ module.exports = {
     `gatsby-plugin-twitter`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-sass`
-  ]
+    `gatsby-plugin-sass`,
+  ],
 }
